20a45e628e844bc97aa4b11d5b3d4a3a06361bae
Diego Andrade <diegoandrade@gmail.com>
2015-03-28 12:43:37 -0400

    Initial Commit

---
+++ Mugen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mugen
+//
+//  Created by Diego Andrade on 3/28/15.
+//  Copyright (c) 2015 CMU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
